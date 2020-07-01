58e4cf921a71f4f45c0b32c29b7a662faed0cd65
Andy Polyakov <appro@cryptogams.org>
2020-07-01 16:51:53 +0200

    bindings/rust/build.rs: switch to assembly.S for real.

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -4,25 +4,11 @@ extern crate cc;
 use std::env;
 use std::path::Path;
 use std::path::PathBuf;
-use std::process::Command;
 
 fn main() {
-    // TODO - could ls directory and find all files
-    let asm_to_build = [
-        "add_mod_256-x86_64",
-        "add_mod_384-x86_64",
-        "mulq_mont_256-x86_64",
-        "mulx_mont_256-x86_64",
-        "sha256-x86_64",
-        "add_mod_384x384-x86_64",
-        "inverse_mod_384-x86_64",
-        "mulq_mont_384-x86_64",
-        "mulx_mont_384-x86_64",
-    ];
-
     let mut file_vec = Vec::new();
 
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let _out_dir = env::var_os("OUT_DIR").unwrap();
 
     let blst_base_dir = match env::var("BLST_SRC_DIR") {
         Ok(val) => val,
@@ -36,23 +22,12 @@ fn main() {
     };
     println!("Using blst source directory {:?}", blst_base_dir);
 
-    let perl_src_dir = blst_base_dir.clone() + "/src/asm/";
     let c_src_dir = blst_base_dir.clone() + "/src/";
+    let build_dir = blst_base_dir.clone() + "/build/";
     let binding_src_dir = blst_base_dir + "/bindings/";
 
-    for a in asm_to_build.iter() {
-        let dest_path = Path::new(&out_dir).join(a).with_extension("s");
-        let src_path = Path::new(&perl_src_dir).join(a).with_extension("pl");
-
-        Command::new(&src_path)
-            .args(&[">", dest_path.to_str().unwrap()])
-            .status()
-            .unwrap();
-
-        file_vec.push(dest_path);
-    }
-
     file_vec.push(Path::new(&c_src_dir).join("server.c"));
+    file_vec.push(Path::new(&build_dir).join("assembly.S"));
 
     // Set CC environment variable to choose alternative C compiler.
     // Optimization level depends on whether or not --release is passed
