a17f8b881601b04f10961b9cc08a6ece515edc96
Andy Polyakov <appro@cryptogams.org>
2020-07-01 10:49:22 +0200

    Update and re-run build/refresh.sh.

--- build/refresh.sh
+++ build/refresh.sh
@@ -10,4 +10,11 @@ for pl in ../src/asm/*-x86_64.pl; do
     (set -x; ${PERL:-perl} $pl mingw64 > coff/$s)
     (set -x; ${PERL:-perl} $pl macosx > mach-o/$s)
 done
-
+for pl in ../src/asm/*-armv8.pl; do
+    s=`basename $pl .pl`.asm
+    (set -x; ${PERL:-perl} $pl win64 > win64/$s)
+    s=`basename $pl .pl`.S
+    (set -x; ${PERL:-perl} $pl linux64 > elf/$s)
+    (set -x; ${PERL:-perl} $pl coff64 > coff/$s)
+    (set -x; ${PERL:-perl} $pl ios64 > mach-o/$s)
+done
