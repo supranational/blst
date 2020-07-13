6d6dec39078e94ef9743a0ac842650a5cd160df5
Andy Polyakov <appro@cryptogams.org>
2020-07-13 11:13:57 +0200

    bindings/blst.h: make blst_scalar_* declarations swig-friendly.

--- bindings/blst.h
+++ bindings/blst.h
@@ -57,14 +57,14 @@ typedef struct { blst_fp fp[2]; } blst_fp2;
 typedef struct { blst_fp2 fp2[3]; } blst_fp6;
 typedef struct { blst_fp6 fp6[2]; } blst_fp12;
 
-void blst_scalar_from_uint32(blst_scalar *ret, const uint32_t a[8]);
-void blst_uint32_from_scalar(uint32_t ret[8], const blst_scalar *a);
-void blst_scalar_from_uint64(blst_scalar *ret, const uint64_t a[4]);
-void blst_uint64_from_scalar(uint64_t ret[4], const blst_scalar *a);
-void blst_scalar_from_bendian(blst_scalar *ret, const byte a[32]);
-void blst_bendian_from_scalar(byte ret[32], const blst_scalar *a);
-void blst_scalar_from_lendian(blst_scalar *ret, const byte a[32]);
-void blst_lendian_from_scalar(byte ret[32], const blst_scalar *a);
+void blst_scalar_from_uint32(blst_scalar *out, const uint32_t a[8]);
+void blst_uint32_from_scalar(uint32_t out[8], const blst_scalar *a);
+void blst_scalar_from_uint64(blst_scalar *out, const uint64_t a[4]);
+void blst_uint64_from_scalar(uint64_t out[4], const blst_scalar *a);
+void blst_scalar_from_bendian(blst_scalar *out, const byte a[32]);
+void blst_bendian_from_scalar(byte out[32], const blst_scalar *a);
+void blst_scalar_from_lendian(blst_scalar *out, const byte a[32]);
+void blst_lendian_from_scalar(byte out[32], const blst_scalar *a);
 bool blst_scalar_fr_check(const blst_scalar *a);
 
 #ifndef SWIG
