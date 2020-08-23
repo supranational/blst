2c7b5fa1c958cf6292573eadfb261c07af168a5c
Andy Polyakov <appro@cryptogams.org>
2020-08-23 16:12:44 +0200

    build/refresh.sh: add build/bindings_trim.pl and execute it.

--- build/refresh.sh
+++ build/refresh.sh
@@ -3,29 +3,30 @@
 HERE=`dirname $0`
 cd "${HERE}"
 
+PERL=${PERL:-perl}
+
 for pl in ../src/asm/*-x86_64.pl; do
     s=`basename $pl .pl`.asm
-    (set -x; ${PERL:-perl} $pl masm > win64/$s)
+    (set -x; ${PERL} $pl masm > win64/$s)
     s=`basename $pl .pl`.s
-    (set -x; ${PERL:-perl} $pl elf > elf/$s)
-    (set -x; ${PERL:-perl} $pl mingw64 > coff/$s)
-    (set -x; ${PERL:-perl} $pl macosx > mach-o/$s)
+    (set -x; ${PERL} $pl elf > elf/$s)
+    (set -x; ${PERL} $pl mingw64 > coff/$s)
+    (set -x; ${PERL} $pl macosx > mach-o/$s)
 done
 
 for pl in ../src/asm/*-armv8.pl; do
     s=`basename $pl .pl`.asm
-    (set -x; ${PERL:-perl} $pl win64 > win64/$s)
+    (set -x; ${PERL} $pl win64 > win64/$s)
     s=`basename $pl .pl`.S
-    (set -x; ${PERL:-perl} $pl linux64 > elf/$s)
-    (set -x; ${PERL:-perl} $pl coff64 > coff/$s)
-    (set -x; ${PERL:-perl} $pl ios64 > mach-o/$s)
+    (set -x; ${PERL} $pl linux64 > elf/$s)
+    (set -x; ${PERL} $pl coff64 > coff/$s)
+    (set -x; ${PERL} $pl ios64 > mach-o/$s)
 done
 
 ( cd ../bindings;
   echo "LIBRARY blst\n\nEXPORTS"
   cc -E blst.h | \
-  ${PERL:-perl} -ne '{ (/(blst_[\w]+)\s*\(/ || /(BLS12_[\w]+);/) && \
-                       print "\t$1\n" }'
+  ${PERL} -ne '{ (/(blst_[\w]+)\s*\(/ || /(BLS12_[\w]+);/) &&  print "\t$1\n" }'
   echo
 ) > win64/blst.def
 
@@ -33,9 +34,10 @@ if which bindgen > /dev/null 2>&1; then
   ( cd ../bindings; set -x;
     bindgen --opaque-type blst_pairing \
             --with-derive-default \
+            --with-derive-eq \
             --size_t-is-usize \
             --rustified-enum BLST.\* \
-        blst.h > rust/src/bindings.rs
+        blst.h | ${PERL} ../build/bindings_trim.pl > rust/src/bindings.rs
   )
 else
     echo "Install Rust bindgen with 'cargo install bindgen'" 1>&2
