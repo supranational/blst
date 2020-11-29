fb9b3b9ba27e2a4051662fd41d5894c02e56ea39
Andy Polyakov <appro@cryptogams.org>
2020-11-29 16:43:06 +0100

    bindings/node.js/blst_wrap.py: fix logical errors.

--- bindings/node.js/blst_wrap.py
+++ bindings/node.js/blst_wrap.py
@@ -9,10 +9,13 @@ try:
     subprocess.check_call(["swig", "-c++", "-javascript",
                                    "-node", "-DV8_VERSION=0x060000",
                                    "-o", sys.argv[2], sys.argv[1]])
+    sys.exit(0)
 except OSError as e:
     if e.errno != 2:    # not "no such file or directory"
         raise e
     sys.exit(e.errno)   # or do something else, say ...
 
 here = re.split(r'[/\\](?=[^/\\]*$)', sys.argv[0])
+if len(here) == 1:
+    here.insert(0, '.')
 shutil.copyfile(here[0] + "/blst_wrap.v12-15.cpp", sys.argv[2])
