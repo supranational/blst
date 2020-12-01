646f1c166cd69fe4f9e07091f33b43cef5ad0b64
Andy Polyakov <appro@cryptogams.org>
2020-12-01 16:59:07 +0100

    bindings/node.js/blst_wrap.py: add template version switch.

--- bindings/node.js/blst_wrap.py
+++ bindings/node.js/blst_wrap.py
@@ -4,11 +4,14 @@ import re
 import shutil
 
 try:
-    version = subprocess.check_output(["swig", "-version"])
-    # analyze 'version' for suitability?
-    subprocess.check_call(["swig", "-c++", "-javascript",
-                                   "-node", "-DV8_VERSION=0x060000",
-                                   "-o", sys.argv[2], sys.argv[1]])
+    version = subprocess.check_output(["swig", "-version"]).decode('ascii')
+    v = re.search(r'SWIG Version ([0-9]+)', version)
+    if v and int(v.group(1)) >= 4:
+        subprocess.check_call(["swig", "-c++", "-javascript",
+                                       "-node", "-DV8_VERSION=0x060000",
+                                       "-o", sys.argv[2], sys.argv[1]])
+    else:
+        raise OSError(2, "unsupported swig version")
     sys.exit(0)
 except OSError as e:
     if e.errno != 2:    # not "no such file or directory"
@@ -18,4 +21,20 @@ except OSError as e:
 here = re.split(r'[/\\](?=[^/\\]*$)', sys.argv[0])
 if len(here) == 1:
     here.insert(0, '.')
-shutil.copyfile(here[0] + "/blst_wrap.v12-15.cpp", sys.argv[2])
+
+version = subprocess.check_output(["node", "--version"]).decode('ascii')
+v = re.match(r'^v([0-9]+)', version)
+if v:
+    maj = int(v.group(1))
+    if maj >= 16:
+        pass
+    elif maj >= 12:
+        pre_gen = "blst_wrap.v12.cpp"
+    elif maj >= 8:
+        pre_gen = "blst_wrap.v8.cpp"
+
+try:
+    shutil.copyfile("{}/{}".format(here[0],pre_gen), sys.argv[2])
+except NameError:
+    sys.stderr.write("unsupported 'node --version': {}".format(version))
+    sys.exit(2)         # "no such file or directory"
