21ff3ef8960ed0175f4d9888ea55153051439911
Andy Polyakov <appro@cryptogams.org>
2021-01-18 22:50:46 +0100

    build.bar: recognize /out:filename option.

--- build.bat
+++ build.bat
@@ -8,10 +8,23 @@ rem FOR %%F IN (%TOP%src\asm\*-x86_64.pl) DO (
 rem     IF NOT EXIST %%~nF.asm (perl %%F masm %%~nF.asm)
 rem )
 rem FOR %%F IN (*.asm) DO (ml64 /nologo /c /Cp /Cx /Zi %%F || EXIT /B)
-FOR %%O IN (%*) DO ( IF "%%O" == "-shared" (
-    cl /nologo /c /O2 /Oi- /MD %TOP%build\win64\dll.c || EXIT /B
-    link /nologo /debug /dll /entry:DllMain /incremental:no /implib:blst.lib /pdb:blst.pdb /def:%TOP%build\win64\blst.def *.obj kernel32.lib && del *.obj
-    EXIT /B
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+set static=/out:blst.lib
+set shared=
+FOR %%O IN (%*) DO (
+    set opt=%%O
+    IF "!opt!" == "-shared" (
+        IF [!shared!] EQU [] set shared=/out:blst.dll
+    ) ELSE IF "!opt:~0,5!" == "/out:" (
+	IF "!opt:~-4!" == ".dll" (set shared=!opt!) ELSE (set static=!opt!)
     )
 )
-lib /nologo /OUT:blst.lib *.obj && del *.obj
+IF [%shared%] NEQ [] (
+    cl /nologo /c /O2 /Oi- /MD %TOP%build\win64\dll.c || EXIT /B
+    link /nologo /debug /dll /entry:DllMain /incremental:no %shared% /def:%TOP%build\win64\blst.def *.obj kernel32.lib && del *.obj
+) ELSE (
+    lib /nologo %static% *.obj && del *.obj
+)
+ENDLOCAL
+EXIT /B
