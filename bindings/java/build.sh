92183f8efd68c94c3c47570046baabe3dca1eac3
Andy Polyakov <appro@cryptogams.org>
2021-02-09 16:48:54 +0100

    bindings/java/run.me: split build procedure into separate script.
    New build.sh creates .jar file with embedded JNI module, which is
    loaded automatically.

---
+++ bindings/java/build.sh
@@ -0,0 +1,71 @@
+#!/bin/sh -e
+
+JAVA_PACKAGE=${JAVA_PACKAGE:-supranational.blst}
+
+TOP=`dirname $0`
+TOP=`(cd $TOP/.. && echo $PWD)`
+
+if [ "x$JAVA_HOME" = "x" -a -x /usr/libexec/java_home ]; then
+    JAVA_HOME=`/usr/libexec/java_home 2>/dev/null || true`
+fi
+if [ "x$JAVA_HOME" = "x" ]; then
+    JAVA_HOME=`java -XshowSettings:properties -version 2>&1 | \
+               awk 'BEGIN { FS="=[ \t]*" } /java\.home/ { print $2 }'`
+fi
+if [ ! -d "$JAVA_HOME" ]; then
+    echo "JAVA_HOME='$JAVA_HOME' is not a directory" 1>&2
+    exit 1
+fi
+if [ ! -d "$JAVA_HOME"/include ]; then
+    if [ -d "$JAVA_HOME"/../include ]; then
+        JAVA_HOME=`dirname "$JAVA_HOME"`
+    else
+        echo "JAVA_HOME='$JAVA_HOME' is not a JDK" 1>&2
+        exit 1
+    fi
+fi
+# spot jni_md.h, which is included from jni.h
+JNI_MD=`ls "$JAVA_HOME"/include/*/jni_md.h`
+JNI_MD=`dirname "$JNI_MD"`
+
+PKG=`echo $JAVA_PACKAGE | tr . /`
+mkdir -p $PKG
+
+if [ ! -f blst_wrap.cpp -o "$TOP"/blst.swg -nt blst_wrap.cpp \
+                        -o "$TOP"/blst.hpp -nt blst_wrap.cpp \
+                        -o "$TOP"/blst.h   -nt blst_wrap.cpp \
+                        -o ! -f $PKG/blst.java ]; then
+    (set -x; swig -c++ -java -package $JAVA_PACKAGE -outdir $PKG \
+                  -o blst_wrap.cpp "$TOP"/blst.swg)
+fi
+
+if [ ! -f $PKG/blst.class -o $PKG/blst.java -nt $PKG/blst.class ]; then
+    (set -x; "$JAVA_HOME"/bin/javac $PKG/*.java)
+fi
+
+# ask blstJNI how does it name the shared library
+SO_NAME=$PKG/`"$JAVA_HOME"/bin/java $PKG/blstJNI 2>/dev/null`
+
+LIBBLST_A=libblst.a
+if [ -f "$TOP"/libblst.a -a "$TOP"/libblst.a -nt "$TOP"/blst.h ]; then
+    LIBBLST_A="$TOP"/libblst.a
+elif [ ! -f libblst.a -o "$TOP"/blst.h -nt libblst.a ]; then
+    $TOP/../build.sh
+fi
+
+if [ ! -f $SO_NAME -o blst_wrap.cpp -nt $SO_NAME \
+                   -o $LIBBLST_A    -nt $SO_NAME ]; then
+    mkdir -p `dirname $SO_NAME`
+    case $SO_NAME in
+        *.so)   LDFLAGS=${LDFLAGS:--Wl,-Bsymbolic};;
+        *.dll)  LDFLAGS=${LDFLAGS:--static-libstdc++};;
+    esac
+    (set -x; ${CXX:-c++} -shared -o $SO_NAME -fPIC \
+                         -I"$JAVA_HOME"/include -I"$JNI_MD" -I"$TOP" \
+                         -O -Wall -Wno-unused-function blst_wrap.cpp \
+                         $LIBBLST_A ${LDFLAGS})
+fi
+
+if [ ! -f $JAVA_PACKAGE.jar -o $SO_NAME -nt $JAVA_PACKAGE.jar ]; then
+    (set -x; jar cf $JAVA_PACKAGE.jar $PKG/*.class $PKG/*/*/*)
+fi
