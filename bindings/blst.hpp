cc293b4ae63922cafa8d4702f8abd2904e474a63
Andy Polyakov <appro@cryptogams.org>
2021-07-31 20:22:38 +0200

    bindings/blst.{h,hpp,swg}: add blst_sk_mul_n_check.

--- bindings/blst.hpp
+++ bindings/blst.hpp
@@ -109,6 +109,8 @@ public:
     {   return blst_sk_add_n_check(&val, &val, a) ? this : nullptr;   }
     Scalar* add(const SecretKey& a)
     {   return blst_sk_add_n_check(&val, &val, &a.key) ? this : nullptr;   }
+    Scalar* mul(const Scalar& a)
+    {   return blst_sk_mul_n_check(&val, &val, a) ? this : nullptr;   }
     Scalar* inverse()
     {   blst_sk_inverse(&val, &val); return this;   }
 
