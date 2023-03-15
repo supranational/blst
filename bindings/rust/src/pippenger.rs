38fbd11e89635c15566b9e326e54b638e5f9eafb
Andy Polyakov <appro@cryptogams.org>
2023-03-15 17:36:12 +0100

    bindings/rust: add pippenger-no_std.rs [and deduplicate tests].

--- bindings/rust/src/pippenger.rs
+++ bindings/rust/src/pippenger.rs
@@ -319,73 +319,19 @@ macro_rules! pippenger_mult_impl {
         }
 
         #[cfg(test)]
-        mod $test_mod {
-            use super::*;
-            use rand::{RngCore, SeedableRng};
-            use rand_chacha::ChaCha20Rng;
-
-            #[test]
-            fn test_mult() {
-                const npoints: usize = 2000;
-                const nbits: usize = 160;
-                const nbytes: usize = (nbits + 7) / 8;
-
-                let mut scalars = Box::new([0u8; nbytes * npoints]);
-                ChaCha20Rng::from_seed([0u8; 32]).fill_bytes(scalars.as_mut());
-
-                let mut points: Vec<$point> = Vec::with_capacity(npoints);
-                unsafe { points.set_len(points.capacity()) };
-
-                let mut naive = <$point>::default();
-                for i in 0..npoints {
-                    unsafe {
-                        let mut t = <$point>::default();
-                        $mult(
-                            &mut points[i],
-                            $generator(),
-                            &scalars[i * nbytes],
-                            core::cmp::min(32, nbits),
-                        );
-                        $mult(&mut t, &points[i], &scalars[i * nbytes], nbits);
-                        $add_or_double(&mut naive, &naive, &t);
-                    }
-                }
-
-                let points = $points::from(&points);
-                assert_eq!(naive, points.mult(scalars.as_ref(), nbits));
-            }
-
-            #[test]
-            fn test_add() {
-                const npoints: usize = 2000;
-                const nbits: usize = 32;
-                const nbytes: usize = (nbits + 7) / 8;
-
-                let mut scalars = Box::new([0u8; nbytes * npoints]);
-                ChaCha20Rng::from_seed([0u8; 32]).fill_bytes(scalars.as_mut());
-
-                let mut points: Vec<$point> = Vec::with_capacity(npoints);
-                unsafe { points.set_len(points.capacity()) };
-
-                let mut naive = <$point>::default();
-                for i in 0..npoints {
-                    unsafe {
-                        $mult(
-                            &mut points[i],
-                            $generator(),
-                            &scalars[i * nbytes],
-                            32,
-                        );
-                        $add_or_double(&mut naive, &naive, &points[i]);
-                    }
-                }
-
-                let points = $points::from(&points);
-                assert_eq!(naive, points.add());
-            }
-        }
+        pippenger_test_mod!(
+            $test_mod,
+            $points,
+            $point,
+            $add_or_double,
+            $generator,
+            $mult,
+        );
     };
 }
+
+#[cfg(test)]
+include!("pippenger-test_mod.rs");
 
 pippenger_mult_impl!(
     p1_affines,
