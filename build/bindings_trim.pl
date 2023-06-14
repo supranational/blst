1d197e747341fb0190a8bda5e0b52b1ee4866593
Andy Polyakov <appro@cryptogams.org>
2023-06-14 16:37:31 +0200

    build/bindings_trim.pl: add types sanity check.

--- build/bindings_trim.pl
+++ build/bindings_trim.pl
@@ -5,6 +5,10 @@ while(<>) { push @file, $_; }
 
 # traverse and remove auto-generated PartialEq for chosen types
 for (my $i = 0; $i <= $#file; $i++) {
+    if (@file[$i] =~ m/pub\s+(?:struct|enum)\s+(\w+)/) {
+        push @structs, $1;
+    }
+
     if (@file[$i] =~ m/struct\s+blst_p[12]/) {
         @file[$i-1] =~ s/,\s*PartialEq//;
     } elsif (@file[$i] =~ m/struct\s+blst_fp12/) {
@@ -21,5 +25,16 @@ for (my $i = 0; $i <= $#file; $i++) {
 }
 
 print @file;
+
+print << '___';
+#[test]
+fn bindgen_test_normal_types() {
+    // from "Rust for Rustaceans" by Jon Gjengset
+    fn is_normal<T: Sized + Send + Sync + Unpin>() {}
+___
+for (@structs) {
+    print "    is_normal::<$_>();\n";
+}
+print "}\n";
 
 close STDOUT;
